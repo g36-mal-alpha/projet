6f901309b553b1d43ee084e41854b568901b282c
Antonin Boutet <boutet-23@hotmail.fr>
2020-05-10 15:44:03 +0200

    Suppression de toutes mention mémo dans la BDD

--- scripts-sql/projet-postgresql/2-data.sql
+++ scripts-sql/projet-postgresql/2-data.sql
@@ -4,7 +4,6 @@ SET search_path TO projet;
 -- Supprimer toutes les données
 DELETE FROM service;
 DELETE FROM concerner;
-DELETE FROM memo;
 DELETE FROM telephone;
 DELETE FROM personne;
 DELETE FROM categorie;
@@ -14,7 +13,7 @@ DELETE FROM compte;
 
 -- Compte
 
-INSERT INTO compte (idcompte, pseudo, motdepasse, email ) VALUES 
+INSERT INTO compte (idcompte, pseudo, motdepasse, email ) VALUES
   (1, 'geek', 'geek', 'geek@3il.fr' ),
   (2, 'chef', 'chef', 'chef@3il.fr' ),
   (3, 'job', 'job', 'job@3il.fr' );
@@ -24,7 +23,7 @@ ALTER TABLE compte ALTER COLUMN idcompte RESTART WITH 4;
 
 -- Role
 
-INSERT INTO role (idcompte, role) VALUES 
+INSERT INTO role (idcompte, role) VALUES
   ( 1, 'ADMINISTRATEUR' ),
   ( 1, 'UTILISATEUR' ),
   ( 2, 'UTILISATEUR' ),
@@ -32,8 +31,8 @@ INSERT INTO role (idcompte, role) VALUES
 
 
 -- Categorie
-  
-INSERT INTO categorie (idcategorie, libelle ) VALUES 
+
+INSERT INTO categorie (idcategorie, libelle ) VALUES
   (1, 'Employés' ),
   (2, 'Partenaires' ),
   (3, 'Clients' ),
@@ -45,7 +44,7 @@ ALTER TABLE categorie ALTER COLUMN idcategorie RESTART WITH 6;
 
 -- Personne
 
-INSERT INTO personne (idpersonne, idcategorie, nom, prenom) VALUES 
+INSERT INTO personne (idpersonne, idcategorie, nom, prenom) VALUES
   ( 1, 1, 'GRASSET', 'Jérôme' ),
   ( 2, 1, 'BOUBY', 'Claude' ),
   ( 3, 1, 'AMBLARD', 'Emmanuel' );
@@ -55,7 +54,7 @@ ALTER TABLE personne ALTER COLUMN idpersonne RESTART WITH 4;
 
 -- Telephone
 
-INSERT INTO telephone (idtelephone, idpersonne, libelle, numero ) VALUES 
+INSERT INTO telephone (idtelephone, idpersonne, libelle, numero ) VALUES
   ( 11, 1, 'Portable', '06 11 11 11 11' ),
   ( 12, 1, 'Fax', '05 55 99 11 11' ),
   ( 13, 1, 'Bureau', '05 55 11 11 11' ),
@@ -69,19 +68,9 @@ INSERT INTO telephone (idtelephone, idpersonne, libelle, numero ) VALUES
 ALTER TABLE telephone ALTER COLUMN idtelephone RESTART WITH 100;
 
 
--- Memo
-
-INSERT INTO memo (idmemo, titre, description, flagurgent, statut, effectif, budget, echeance, idcategorie ) VALUES 
-  ( 1, 'Mémo n°1', 'Texte du mémo n°1', TRUE,  2,   2,   1234.56,   {d  '2020-02-25' }, 1 ),
-  ( 2, 'Mémo n°2', 'Texte du mémo n°2', FALSE, 1,   4,   5000.00,   {d  '2020-05-18' }, 2 ),
-  ( 3, 'Mémo n°3', NULL, TRUE, 0, NULL, NULL, NULL, NULL );
-
-ALTER TABLE memo ALTER COLUMN idmemo RESTART WITH 4;
-
-
 -- Concerner
 
-INSERT INTO concerner (idmemo, idPersonne) VALUES 
+INSERT INTO concerner (idmemo, idPersonne) VALUES
   ( 1, 1 ),
   ( 1, 2 ),
   ( 1, 3 ),
@@ -91,7 +80,7 @@ INSERT INTO concerner (idmemo, idPersonne) VALUES
 
 -- Service
 
-INSERT INTO service ( idservice, nom, anneecreation, flagsiege ) VALUES 
+INSERT INTO service ( idservice, nom, anneecreation, flagsiege ) VALUES
   ( 1, 'Direction', 2007, TRUE ),
   ( 2, 'Comptabilité', NULL, TRUE ),
   ( 3, 'Agence Limoges', 2008, FALSE ),
@@ -99,4 +88,3 @@ INSERT INTO service ( idservice, nom, anneecreation, flagsiege ) VALUES
 
 
 ALTER TABLE service ALTER COLUMN idservice RESTART WITH 5;
-
