acaa555af03dbd56ada208dd90f5fd6282987576
Maxime Monoury <monourymaxime18@gmail.com>
2020-05-12 11:38:32 +0200

    modif de 2/3 noms de colonne dans table + ajout de data en fonction des tables

--- scripts-sql/projet-postgresql/2-data.sql
+++ scripts-sql/projet-postgresql/2-data.sql
@@ -75,5 +75,51 @@ INSERT INTO service ( idservice, nom, anneecreation, flagsiege ) VALUES
   ( 3, 'Agence Limoges', 2008, FALSE ),
   ( 4, 'Agence Brive', 2014, FALSE );
 
+  
+ /*
+-- Bénévole 
+
+  INSERT INTO benevole ( idbenevole, nom, prenom, age, permis_conduire, mineurs, permanent ) VALUES
+  ( 1, 'nom1', 'bob', 34,'lien_fichier1' ,TRUE, TRUE),
+  ( 2, 'nom2', 'paul', 25,'lien_fichier1', TRUE, FALSE),
+  ( 3, 'nom3', 'jack', 36,'lien_fichier1', FALSE, TRUE ),
+  ( 4, 'nom4', 'bouss', 45,'lien_fichier1', FALSE, TRUE );
+
+-- Poste 
+
+  INSERT INTO poste ( idposte, lieu, signaleur, horaires, numero_poste ) VALUES
+  ( 1, 'lieu1', TRUE,  '17:00:00' , 1),
+  ( 2, 'lieu2', FALSE, '18:00:00' , 22),
+  ( 3, 'lieu3', TRUE,  '09:00:00' , 17),
+  ( 4, 'lieu4', TRUE , '07:00:00' , 45);
+  
+-- Equipe
+
+  INSERT INTO equipe ( idequipe, nom_equipe) VALUES
+  ( 1, 'equipe1'),
+  ( 2, 'equipe2');
+
+-- Epreuve
+
+  INSERT INTO epreuve ( idepreuve, nom, date_epreuve, lieu) VALUES
+  ( 1, 'epreuve a', '12/08/2020',  'lieu 1'),
+  ( 2, 'epreuve b', '17/08/2020', 'lieu 2'),
+  ( 3, 'epreuve c', '11/08/2020',  'lieu 1');
+
+-- Participant
+ 
+  INSERT INTO paticipant ( idparticpant, nom, prenom,, age, sexe, numero_tel,  date_naissance, adresse, role, certificat_medical, mail, niveau, materiel_utilise, nb_plateaux) VALUES
+  ( 1,'nom1', 'leo', 25, 'M', '06 52 36 98 25','12/08/1995', 'adresse 1', 'chef equipe','fichier 1', 'mail1@gmail.com',  'junior' , 'velo, kanoé',  1),
+  ( 2, 'nom2','bob', 35, 'M', '06 95 58 21 02','12/08/1965', 'adresse 2', 'equipier',   'fichier 2', 'mail2@hotmail.fr', 'senior', 'velo', 2),
+  ( 3,'nom3', 'toto', 22,'M', '06 87 10 96 32','12/08/1998','adresse 3', 'equipier',   'fichier 3', 'mail3@orange.fr',  'junior', 'kanoe' , 2);
+  
+-- Documents Licncies
+
+  INSERT INTO documents_licencies ( iddocument, nom_doc, date_doc, doc) VALUES
+  ( 1, 'doc a', '12/08/2020',  'lien 1'),
+  ( 2, 'doc b', '17/08/2020', 'lien 2'),
+  ( 3, 'doc c', '11/08/2020',  'lien 1');
+
+*/
 
 ALTER TABLE service ALTER COLUMN idservice RESTART WITH 5;
