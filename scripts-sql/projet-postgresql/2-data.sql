01a3d46664fe3a39bdd58551396737bb60cc2c6e
Maxime Monoury <monourymaxime18@gmail.com>
2020-05-12 14:27:24 +0200

    patch data.sql et tables.sql

--- scripts-sql/projet-postgresql/2-data.sql
+++ scripts-sql/projet-postgresql/2-data.sql
@@ -7,7 +7,17 @@ DELETE FROM telephone;
 DELETE FROM personne;
 DELETE FROM categorie;
 DELETE FROM role;
+DELETE FROM benevole;
 DELETE FROM compte;
+
+
+DELETE FROM poste;
+DELETE FROM equipe;
+DELETE FROM epreuve;
+DELETE FROM participant;
+DELETE FROM documents_licencies;
+DELETE FROM avoir;
+DELETE FROM encadrer;
 
 
 -- Compte
@@ -75,51 +85,61 @@ INSERT INTO service ( idservice, nom, anneecreation, flagsiege ) VALUES
   ( 3, 'Agence Limoges', 2008, FALSE ),
   ( 4, 'Agence Brive', 2014, FALSE );
 
-  
- /*
+ALTER TABLE service ALTER COLUMN idservice RESTART WITH 5;
+ 
 -- Bénévole 
 
-  INSERT INTO benevole ( idbenevole, nom, prenom, age, permis_conduire, mineurs, permanent ) VALUES
-  ( 1, 'nom1', 'bob', 34,'lien_fichier1' ,TRUE, TRUE),
-  ( 2, 'nom2', 'paul', 25,'lien_fichier1', TRUE, FALSE),
-  ( 3, 'nom3', 'jack', 36,'lien_fichier1', FALSE, TRUE ),
-  ( 4, 'nom4', 'bouss', 45,'lien_fichier1', FALSE, TRUE );
+INSERT INTO benevole ( idbenevole, idcompte, nom, prenom, age, permis_conduire, mineurs, permanent ) VALUES
+  ( 1,1, 'nom1', 'bob', 34,'lien_fichier1' ,TRUE, TRUE),
+  ( 2,2, 'nom2', 'paul', 25,'lien_fichier1', TRUE, FALSE),
+  ( 3,3, 'nom3', 'jack', 36,'lien_fichier1', FALSE, TRUE );
+  
+ALTER TABLE benevole ALTER COLUMN idbenevole RESTART WITH 8;
 
 -- Poste 
 
-  INSERT INTO poste ( idposte, lieu, signaleur, horaires, numero_poste ) VALUES
-  ( 1, 'lieu1', TRUE,  '17:00:00' , 1),
-  ( 2, 'lieu2', FALSE, '18:00:00' , 22),
-  ( 3, 'lieu3', TRUE,  '09:00:00' , 17),
-  ( 4, 'lieu4', TRUE , '07:00:00' , 45);
+INSERT INTO poste ( idposte, lieu, signaleur, horaires, numero_poste ) VALUES
+  ( 20, 'lieu1', TRUE,  '17:00:00' , 1),
+  ( 30, 'lieu2', FALSE, '18:00:00' , 22),
+  ( 40, 'lieu3', TRUE,  '09:00:00' , 17),
+  ( 50, 'lieu4', TRUE , '07:00:00' , 45);
+ 
+ALTER TABLE poste ALTER COLUMN idposte RESTART WITH 5;
   
 -- Equipe
 
-  INSERT INTO equipe ( idequipe, nom_equipe) VALUES
+INSERT INTO equipe ( idequipe, nom_equipe) VALUES
   ( 1, 'equipe1'),
-  ( 2, 'equipe2');
+  ( 2, 'equipe2'),
+  ( 3, 'equipe 3');
 
+ALTER TABLE equipe ALTER COLUMN idequipe RESTART WITH 2;
+  
 -- Epreuve
 
-  INSERT INTO epreuve ( idepreuve, nom, date_epreuve, lieu) VALUES
+INSERT INTO epreuve ( idepreuve, nom, date_epreuve, lieu) VALUES
   ( 1, 'epreuve a', '12/08/2020',  'lieu 1'),
   ( 2, 'epreuve b', '17/08/2020', 'lieu 2'),
   ( 3, 'epreuve c', '11/08/2020',  'lieu 1');
+  
+ALTER TABLE epreuve ALTER COLUMN idepreuve RESTART WITH 4;
 
 -- Participant
  
-  INSERT INTO paticipant ( idparticpant, nom, prenom,, age, sexe, numero_tel,  date_naissance, adresse, role, certificat_medical, mail, niveau, materiel_utilise, nb_plateaux) VALUES
-  ( 1,'nom1', 'leo', 25, 'M', '06 52 36 98 25','12/08/1995', 'adresse 1', 'chef equipe','fichier 1', 'mail1@gmail.com',  'junior' , 'velo, kanoé',  1),
-  ( 2, 'nom2','bob', 35, 'M', '06 95 58 21 02','12/08/1965', 'adresse 2', 'equipier',   'fichier 2', 'mail2@hotmail.fr', 'senior', 'velo', 2),
-  ( 3,'nom3', 'toto', 22,'M', '06 87 10 96 32','12/08/1998','adresse 3', 'equipier',   'fichier 3', 'mail3@orange.fr',  'junior', 'kanoe' , 2);
+INSERT INTO participant (idparticipant, idequipe, idepreuve, idcompte, nom, prenom, age, sexe, numero_tel,  date_naissance, adresse, role, certificat_medical, mail, niveau, materiel_utilise, nb_plateaux) VALUES
+  ( 1,1,1,1, 'nom1', 'leo', 25, 'M', '06 52 36 98 25','12/08/1995', 'adresse 1', 'chef equipe','fichier 1', 'mail1@gmail.com',  'junior' , 'velo, kanoé',  1),
+  ( 2,2,2,2, 'nom2', 'bob', 35, 'M', '06 95 58 21 02','12/08/1965', 'adresse 2', 'equipier',   'fichier 2', 'mail2@hotmail.fr', 'senior', 'velo', 2),
+  ( 3,3,3,3, 'nom3', 'toto', 22,'M', '06 87 10 96 32','12/08/1998','adresse 3', 'equipier',   'fichier 3', 'mail3@orange.fr',  'junior', 'kanoe' , 2);
   
+ALTER TABLE participant ALTER COLUMN idparticipant RESTART WITH 17;
+ 
 -- Documents Licncies
 
-  INSERT INTO documents_licencies ( iddocument, nom_doc, date_doc, doc) VALUES
-  ( 1, 'doc a', '12/08/2020',  'lien 1'),
-  ( 2, 'doc b', '17/08/2020', 'lien 2'),
-  ( 3, 'doc c', '11/08/2020',  'lien 1');
+INSERT INTO documents_licencies ( iddocument, idparticipant, nom_doc, date_doc, doc) VALUES
+  ( 1, 1, 'doc a', '12/08/2020',  'lien 1'),
+  ( 2, 2, 'doc b', '17/08/2020', 'lien 2'),
+  ( 3, 3, 'doc c', '11/08/2020',  'lien 1');
 
-*/
+ALTER TABLE documents_licencies ALTER COLUMN iddocument RESTART WITH 5;
 
-ALTER TABLE service ALTER COLUMN idservice RESTART WITH 5;
+
