31e16922a9ac4b1dbfa920d5e95b5128550a0ebb
Antonin Boutet <boutet-23@hotmail.fr>
2020-05-18 11:50:26 +0200

    Ajout deuxième pdf dans menu

--- src/projet/report/EnumReport.java
+++ src/projet/report/EnumReport.java
@@ -7,6 +7,7 @@ public enum EnumReport implements IEnumReport {
 	// Valeurs
 	
 	ParticipantsList		( "participants/participantList.jrxml" ),
+	BenevolesList			( "benevoles/benevoleList.jrxml" ),
 	;
 
 	
