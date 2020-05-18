ebcc37a9798726ea8098ef4dc12519e43a5457b9
LucasBOUSSARD <Lucas BOUSSARD@LAPTOP-LUCAS>
2020-05-18 16:21:39 +0200

    Info participant liaison label code

--- src/projet/view/participant/ControllerInfoParticipant.java
+++ src/projet/view/participant/ControllerInfoParticipant.java
@@ -12,9 +12,26 @@ public class ControllerInfoParticipant {
 	@FXML
 	private Label		textNom;
 	@FXML
-
-
-	
+	private Label		textPoste;
+	@FXML
+	private Label		textPrenom;
+	@FXML
+	private Label		textTelephone;
+	@FXML
+	private Label		textAdresseMail;
+	@FXML
+	private Label		textAdresse;
+	@FXML
+	private Label		textCourse;
+	@FXML
+	private Label		textPermis;
+	@FXML
+	private Label		textMajeur;
+	@FXML
+	private Label		textCertificat;
+	@FXML
+	private Label		textCout;
+		
 	// Autres champs
 	
 	@Inject
@@ -28,8 +45,27 @@ public class ControllerInfoParticipant {
 		
 		// Data binding
 		textNom.textProperty().bind( modelInfoParticipant.nomProperty() );
-		modelInfoParticipant.nomProperty().setValue( "Bienvenue" );
-	
+		modelInfoParticipant.nomProperty().setValue( "Nom : %d " );
+		textPoste.textProperty().bind( modelInfoParticipant.posteProperty() );
+		modelInfoParticipant.posteProperty().setValue( "Poste : " );
+		textPrenom.textProperty().bind( modelInfoParticipant.prenomProperty() );
+		modelInfoParticipant.prenomProperty().setValue( "Prenom : " );
+		textTelephone.textProperty().bind( modelInfoParticipant.telephoneProperty() );
+		modelInfoParticipant.telephoneProperty().setValue( "Téléphone : " );
+		textAdresseMail.textProperty().bind( modelInfoParticipant.adressemailProperty() );
+		modelInfoParticipant.adressemailProperty().setValue( "Adresse Mail : " );
+		textAdresse.textProperty().bind( modelInfoParticipant.adresseProperty() );
+		modelInfoParticipant.adresseProperty().setValue( "Adresse :" );
+		textCourse.textProperty().bind( modelInfoParticipant.adresseProperty() );
+		modelInfoParticipant.courseProperty().setValue( "Course : " );
+		textPermis.textProperty().bind( modelInfoParticipant.courseProperty() );
+		modelInfoParticipant.permisProperty().setValue( "Permis : " );
+		textMajeur.textProperty().bind( modelInfoParticipant.majeurProperty() );
+		modelInfoParticipant.majeurProperty().setValue( "Majeur : " );
+		textCertificat.textProperty().bind( modelInfoParticipant.certificatProperty() );
+		modelInfoParticipant.certificatProperty().setValue( "Certificat Médicale : " );
+		textCout.textProperty().bind( modelInfoParticipant.coutProperty() );
+		modelInfoParticipant.coutProperty().setValue( "Coût : " );
 		
 	}
 
--- src/projet/view/participant/ModelInfoParticipant.java
+++ src/projet/view/participant/ModelInfoParticipant.java
@@ -2,14 +2,23 @@ package projet.view.participant;
 
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
+import projet.data.Participant;
 
 public class ModelInfoParticipant {
 	
 	// DonnÃ©es observables 
 	
 	private final Property<String>	nom	= new SimpleObjectProperty<>();
-	private final Property<String>	message	= new SimpleObjectProperty<>();
-	
+	private final Property<String>	poste	= new SimpleObjectProperty<>();
+	private final Property<String>	prenom	= new SimpleObjectProperty<>();
+	private final Property<String>	telephone	= new SimpleObjectProperty<>();
+	private final Property<String>	adressemail	= new SimpleObjectProperty<>();
+	private final Property<String>	adresse	= new SimpleObjectProperty<>();
+	private final Property<String>	course	= new SimpleObjectProperty<>();
+	private final Property<String>	permis	= new SimpleObjectProperty<>();
+	private final Property<String>	majeur	= new SimpleObjectProperty<>();
+	private final Property<String>	certificat	= new SimpleObjectProperty<>();
+	private final Property<String>	cout	= new SimpleObjectProperty<>();
 
 	// Getters 
 	
@@ -17,9 +26,46 @@ public class ModelInfoParticipant {
 		return nom;
 	}
 	
-	public Property<String> messageProperty() {
-		return message;
+	public Property<String> posteProperty() {
+		return poste;
 	}
 	
+	public Property<String> prenomProperty() {
+		return prenom;
+	}
+	
+	public Property<String> telephoneProperty() {
+		return telephone;
+	}
+	
+	public Property<String> adressemailProperty() {
+		return adressemail;
+	}
+	
+	public Property<String> adresseProperty() {
+		return adresse;
+	}
+	
+	public Property<String> courseProperty() {
+		return course;
+	}
+	
+	public Property<String> permisProperty() {
+		return permis;
+	}
+	
+	public Property<String> majeurProperty() {
+		return majeur;
+	}
+	
+	public Property<String> certificatProperty() {
+		return certificat;
+	}
+	
+	public Property<String> coutProperty() {
+		return cout;
+	}
+
+
 
 }
