a06df37ed0a77631e11ecd986aa32f459ee15eca
Maxime Monoury <monourymaxime18@gmail.com>
2020-05-20 21:56:10 +0200

    création projet.data poste

---
+++ src/projet/data/Poste.java
@@ -0,0 +1,141 @@
+package projet.data;
+
+
+import java.time.LocalTime;
+import java.util.Objects;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class Poste {
+	
+	
+	// Champs
+	
+	private final Property<Integer>		id				= new SimpleObjectProperty<>();
+	private final StringProperty   	 	libelle     	= new SimpleStringProperty();
+	private final StringProperty   	 	lieu   	        = new SimpleStringProperty();
+	private final Property<LocalTime>   heure_debut     = new SimpleObjectProperty<>();
+	private final Property<LocalTime>   heure_fin       = new SimpleObjectProperty<>();
+	private final Property<Integer>		numero_poste    = new SimpleObjectProperty<>();
+
+
+	
+	// Getters & setters
+
+	public final Property<Integer> idProperty() {
+		return this.id;
+	}
+	
+	public final Integer getId() {
+		return this.idProperty().getValue();
+	}
+	
+	public final void setId(final Integer id) {
+		this.idProperty().setValue(id);
+	}
+	
+	public final StringProperty libelleProperty() {
+		return this.libelle;
+	}
+	
+
+	public final String getLibelle() {
+		return this.libelleProperty().get();
+	}
+	
+
+	public final void setLibelle(final String libelle) {
+		this.libelleProperty().set(libelle);
+	}
+	
+
+	public final StringProperty lieuProperty() {
+		return this.lieu;
+	}
+	
+
+	public final String getLieu() {
+		return this.lieuProperty().get();
+	}
+	
+
+	public final void setLieu(final String lieu) {
+		this.lieuProperty().set(lieu);
+	}
+	
+
+	public final Property<LocalTime> heure_debutProperty() {
+		return this.heure_debut;
+	}
+	
+
+	public final LocalTime getHeure_debut() {
+		return this.heure_debutProperty().getValue();
+	}
+	
+
+	public final void setHeure_debut(final LocalTime heure_debut) {
+		this.heure_debutProperty().setValue(heure_debut);
+	}
+	
+
+	public final Property<LocalTime> heure_finProperty() {
+		return this.heure_fin;
+	}
+	
+
+	public final LocalTime getHeure_fin() {
+		return this.heure_finProperty().getValue();
+	}
+	
+
+	public final void setHeure_fin(final LocalTime heure_fin) {
+		this.heure_finProperty().setValue(heure_fin);
+	}
+	
+
+	public final Property<Integer> numero_posteProperty() {
+		return this.numero_poste;
+	}
+	
+
+	public final Integer getNumero_poste() {
+		return this.numero_posteProperty().getValue();
+	}
+	
+
+	public final void setNumero_poste(final Integer numero_poste) {
+		this.numero_posteProperty().setValue(numero_poste);
+	}
+	
+	// hashCode() & equals()
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id.getValue());
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Poste other = (Poste) obj;
+		return Objects.equals(id.getValue(), other.id.getValue());
+	}
+
+	
+	// toString()
+	
+	@Override
+	public String toString() {
+		return getLibelle();
+	}
+}
