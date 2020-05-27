93a1383461288500eafd3382b216197587a078f2
Maxime Monoury <monourymaxime18@gmail.com>
2020-05-27 20:40:40 +0200

    patch donnée " buvette"

--- scripts-sql/projet-postgresql/2-data.sql
+++ scripts-sql/projet-postgresql/2-data.sql
@@ -97,7 +97,7 @@ ALTER TABLE benevole ALTER COLUMN idbenevole RESTART WITH 8;
 
 INSERT INTO poste ( idposte, lieu, libelle, statut ,jour, heure_debut, numero_poste, heure_fin, idcategorie) VALUES
   ( 1, 'lieu1', 'signaleur',1,'12/08/2020', '17:00:00' , 1, '18:00:00', 1),
-  ( 2, 'lieu2', 'buvette',2,'12/08/2020', '18:00:00' , 22, '19:00:00' , 2),
+  ( 2, 'lieu2', 'buvette',1,'12/08/2020', '17:00:00' , 2, '18:00:00', 1),
   ( 3, 'lieu3', 'moto',1,'12/08/2020' ,  '09:00:00' , 17, '11:00:00', 1),
   ( 4, 'lieu4', 'photographe',1, '12/08/2020', '07:00:00' , 45, '10:00:00', 1);
  
